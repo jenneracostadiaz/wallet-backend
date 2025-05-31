6346e2382d608c2bbcc57d9f0ce45d9516db6339
C. Jenner Acosta Díaz <jenner.acosta@itware.com.pe>
2025-05-31 15:09:36 -0500

    feat(accounts): create accounts type

---
+++ resources/js/types/accounts.ts
@@ -0,0 +1,13 @@
+export type Account = {
+    id: number,
+    name: string,
+    description: string,
+    balance: number,
+    currency_id: number,
+    user_id: number,
+    type: string,
+    order: string,
+    created_at: string,
+    updated_at: string,
+}
+
