7b259066dec33e63c6d1bb85b954c7cc8149ec9a
C. Jenner Acosta Díaz <jenner.acosta@itware.com.pe>
2025-05-31 21:04:08 -0500

    feat(accounts): add type in card

--- resources/js/pages/accounts/index.tsx
+++ resources/js/pages/accounts/index.tsx
@@ -4,8 +4,8 @@ import AppLayout from '@/layouts/app-layout';
 import Heading from '@/components/heading';
 import { Account } from '@/types/accounts';
 import HeadingSmall from '@/components/heading-small';
-import { Pencil, PiggyBank, Trash2 } from 'lucide-react';
-import { Button } from '@/components/ui/button';
+import { Pencil, Trash2 } from 'lucide-react';
+import { useAccountTypeColor, useAccountTypeName } from '@/hooks/use-account-types';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -20,54 +20,23 @@ interface AccountProps {
 
 export default function index({accounts}: AccountProps) {
 
-    // Create color for types: checking, savings, credit card, cash
-    const accountTypes = ['checking', 'savings', 'credit_card', 'cash'];
-    const accountColors = ['bg-teal-400', 'bg-blue-400', 'bg-red-400', 'bg-yellow-400'];
-    const accountIcons = ['credit-card', 'piggy-bank', 'bank-note', 'cash-register'];
-    const accountNames = ['Checking', 'Savings', 'Credit Card', 'Cash'];
-
-    const accountTypesWithIcons = accountTypes.map((type, index) => {
-        return {
-            type,
-            icon: accountIcons[index],
-            color: accountColors[index],
-            name: accountNames[index]
-        }
-    })
-
-    console.log(accounts);
-
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Accounts" />
             <div className="px-4 py-6">
                 <Heading title="Accounts" description="Manage your bank accounts, create new ones, and more." />
 
-                <ul className="grid gap-4">
+                <ul className="grid gap-4 grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3">
                     {accounts.map((account: Account) => (
-                        <li key={account.id} className="flex gap-2 border border-sidebar-border/70 dark:border-sidebar-border rounded-xl py-3 px-4">
-                            <div className="flex-1"><HeadingSmall title={account.name} description={account.description} /></div>
-                            <div className="flex-1">
-                                <p className="text-muted-foreground text-xs uppercase">
-                                    {accountTypesWithIcons.map((type, index) => {
-                                        if (account.type === type.type) {
-                                            return (
-                                                <div key={index} className={`${type.color} text-white text-xs rounded-full px-2 py-1 mr-1 flex items-center gap-1 justify-center`}>
-                                                    <PiggyBank />
-                                                    {type.name}
-                                                </div>
-                                            )
-                                        }
-                                    })}
-                                </p>
-                            </div>
-                            <div className="flex-1">
+                        <li key={account.id} className={`flex items-center gap-4 relative border-3 ${useAccountTypeColor(account.type)} rounded-xl py-3 px-4`}>
+                            <span className="flex-1"><HeadingSmall title={account.name} smallTitle={useAccountTypeName(account.type)} description={account.description} /></span>
+                            <span className="">
                                 <p className="text-muted-foreground text-sm">{account.balance}</p>
-                            </div>
-                            <div className="flex-1 flex items-center justify-end gap-3">
-                                <Button variant="default" size="xs" className="bg-teal-400 dark:bg-teal-800"><Pencil /></Button>
-                                <Button variant="destructive" size="xs"><Trash2 size="5" /></Button>
-                            </div>
+                            </span>
+                            <span className="flex items-center justify-end gap-2">
+                                <button className="bg-teal-400 dark:bg-teal-800 hover:bg-teal-200 p-1 rounded cursor-pointer"><Pencil size={12} /></button>
+                                <button className="bg-red-400 dark:bg-red-800 p-1 rounded cursor-pointer"><Trash2 size={12} /></button>
+                            </span>
                         </li>
                     ))}
                 </ul>
