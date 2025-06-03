0107338a1728274e36a2b229c16603bab332b812
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-06-03 02:24:26 -0500

    feat(accounts): add delete functionality with confirmation prompt
    Introduced delete functionality in the accounts list with a confirmation prompt. Integrated `router.delete` for account removal and added a processing state to handle UI updates during deletion. Refactored account rendering to improve clarity and usability.

--- resources/js/pages/accounts/index.tsx
+++ resources/js/pages/accounts/index.tsx
@@ -1,11 +1,11 @@
-import { Head } from '@inertiajs/react'
+import { Head, router } from '@inertiajs/react';
 import type { BreadcrumbItem } from '@/types';
 import AppLayout from '@/layouts/app-layout';
 import Heading from '@/components/heading';
 import { Account } from '@/types/accounts';
-import HeadingSmall from '@/components/heading-small';
 import { Pencil, Trash2 } from 'lucide-react';
-import { useAccountTypeColor, useAccountTypeName } from '@/hooks/use-account-types';
+import { getAccountTypeColor } from '@/hooks/use-account-types';
+import { useState } from 'react';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -18,7 +18,17 @@ interface AccountProps {
     accounts: Account[];
 }
 
-export default function index({accounts}: AccountProps) {
+export default function Index({accounts}: AccountProps) {
+    const [processing, setProcessing] = useState(false);
+
+    const handleDelete = (id:number) => {
+        if (confirm('Are you sure you want to delete this post?')) {
+            setProcessing(true);
+            router.delete(`/accounts/${id}`, {
+                onFinish: () => setProcessing(false),
+            });
+        }
+    };
 
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
@@ -28,14 +38,25 @@ export default function index({accounts}: AccountProps) {
 
                 <ul className="grid gap-4 grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3">
                     {accounts.map((account: Account) => (
-                        <li key={account.id} className={`flex items-center gap-4 relative border-3 ${useAccountTypeColor(account.type)} rounded-xl py-3 px-4`}>
-                            <span className="flex-1"><HeadingSmall title={account.name} smallTitle={useAccountTypeName(account.type)} description={account.description} /></span>
+                        <li key={account.id} className={`flex items-center gap-4 relative border-3 ${getAccountTypeColor(account.type)} rounded-xl py-3 px-4`}>
+                            <span className="flex-1">
+                                <h3 className="mb-0.5 text-base font-medium">
+                                    {account.name} {account.type && <span className="text-xs text-muted-foreground">({account.type})</span>}
+                                </h3>
+                                {account.description && <p className="text-muted-foreground text-sm">{account.description}</p>}
+                            </span>
                             <span className="">
                                 <p className="text-muted-foreground text-sm">{account.balance}</p>
                             </span>
                             <span className="flex items-center justify-end gap-2">
                                 <button className="bg-teal-400 dark:bg-teal-800 hover:bg-teal-200 p-1 rounded cursor-pointer"><Pencil size={12} /></button>
-                                <button className="bg-red-400 dark:bg-red-800 p-1 rounded cursor-pointer"><Trash2 size={12} /></button>
+                                <button
+                                    className="bg-red-400 dark:bg-red-800 p-1 rounded cursor-pointer"
+                                    onClick={() => handleDelete(account.id)}
+                                    disabled={processing}
+                                >
+                                    <Trash2 size={12} />
+                                </button>
                             </span>
                         </li>
                     ))}
