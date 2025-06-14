c4e92974639b9bbcfe420ecbbd954c25fe1e8683
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-06-14 13:54:06 -0500

    feat(accounts): add account seeder

---
+++ database/seeders/AccountSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Account;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AccountSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::all()->each(function ($user) {
+            $user->accounts()->saveMany(Account::factory(10)->make());
+        });
+    }
+}
