8941006c3507e1b8079fc8a323bd026a62129c26
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-06-15 14:11:26 -0500

    feat(transactions): update delete policy in TransactionPolicy to allow deletion regardless of category

--- app/Policies/TransactionPolicy.php
+++ app/Policies/TransactionPolicy.php
@@ -26,7 +26,7 @@ class TransactionPolicy
      */
     public function delete(User $user, Transaction $transaction): bool
     {
-        return $user->id === $transaction->user_id && $transaction->category_id === null;
+        return $user->id === $transaction->user_id;
     }
 
     /**
