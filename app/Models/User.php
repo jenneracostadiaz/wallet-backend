8125bd21b71c326a23e46875c54db4997e64ecdc
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-06-16 21:16:57 -0500

    feat(user): add PHPDoc properties for User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,9 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * @property int $id
+ */
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
